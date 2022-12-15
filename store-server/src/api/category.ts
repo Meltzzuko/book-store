5c989c81d1ac03a15c5863cb84ed3fd140c78405
นฤเบศร โสพรรณรัตน์ <bosschikung@gmail.com>
2022-12-15 14:14:30 +0700

    เพิ่ม method getAll getSingle

--- store-server/src/api/category.ts
+++ store-server/src/api/category.ts
@@ -1,16 +1,22 @@
 import Router from "koa-router";
 import db from '../db';
-
 const router = new Router()
 
 const makeQuery = () => db('category').select('*')
 const findById = (id: number) => makeQuery().where({id})
 
-router.get('/', async (ctx, next) => {
-    ctx.body = [
-        {id: 1, title: 'Programming'},
-        {id: 2, title: 'Life Style'}
-    ]
-})
+router  
+    .get('/', async (ctx, next) => {
+        ctx.body = await makeQuery().orderBy('id')
+    })
+    .get('/:id', async (ctx, next) => {
+        const id = parseInt(ctx.params.id)
+        const category = await findById(id).first()
+        if(!category){
+            ctx.response.status = 404
+        }
+        ctx.body = category
+    })
+
 
 export default router
